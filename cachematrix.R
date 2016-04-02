ec967ecf8c81c5020c156300e987710d50152d07
ohk1st <ohk1st@gmail.com>
2016-04-02 03:35:05 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,6 @@
+## Hi, the following fucntions create a matrix, inverse it and store it in cache for timely processing
+
+## MakeCacheMatrix is a function that returns a list of cached functions
 makeCacheMatrix<-function(x=matrix()) {
         inverse<-NULL
         choose<-function(y) {
@@ -11,6 +14,8 @@ makeCacheMatrix<-function(x=matrix()) {
         chooseinverse=chooseinverse,getinverse=getinverse)
 }
 
+## Calculates the inverse of the matrix returned by the previous function and retrieves it from cache
+
 cacheSolve<-function(x, ...){
         inverse<-x$getinverse()
         if(!is.null(inverse)){
@@ -21,3 +26,11 @@ cacheSolve<-function(x, ...){
         x$chooseinverse(inverse)
         inverse
         }
+        
+## You can use the following code to test my code.
+mymatrix<-makeCacheMatrix(matrix(1:4, 2, 2))
+mymatrix$get()
+mymatrix$getinverse()
+cacheSolve(mymatrix)
+
+## Thanks
